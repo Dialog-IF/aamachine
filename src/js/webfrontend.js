3f2656324b3d662174b8a98be2ce346add8e13bd
Daniel Stelzer <dsdraco7@gmail.com>
2025-06-12 15:36:48 -0500

    add fadeout

--- src/js/webfrontend.js
+++ src/js/webfrontend.js
@@ -939,14 +939,22 @@ window.run_game = function(story64, options) {
 				} else {
 					chan = "main";
 				}
-				if(chan in this.audio) { // Channel already exists
-					this.audio[chan].pause();
-					this.audio[chan].removeAttribute("src");
-					this.audio[chan].load();
+				if(chan in this.audio && !this.audio[chan].ended) { // Something is currently playing on this channel, we need to stop it
+					let duration = 500;
+					$(this.audio[chan]).animate({volume:0}, duration); // Fade out the existing audio
+					setTimeout(function(t, url, loop){ // Start the new audio once the fade is done
+						t.audio[chan].pause(); // Stop the old audio object completely
+						t.audio[chan].removeAttribute("src");
+						t.audio[chan].load();
+						t.audio[chan] = new Audio(url); // Start the new one
+						t.audio[chan].play();
+						t.audio[chan].loop = loop;
+					}, duration, this, this.transform_url(res.url), !!res.options.match(/\bloop\b/));
+				} else { // TODO clean this up a bit
+					this.audio[chan] = new Audio(this.transform_url(res.url));
+					this.audio[chan].play();
+					this.audio[chan].loop = !!res.options.match(/\bloop\b/);
 				}
-				this.audio[chan] = new Audio(this.transform_url(res.url));
-				this.audio[chan].play();
-				this.audio[chan].loop = !!res.options.match(/\bloop\b/);
 			} else { // Anything else is not recognized
 				this.print("[");
 				this.print(res.alt);
