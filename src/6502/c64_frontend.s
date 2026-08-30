3d08a18b91c3b7c500a8ac019354167d6ed84440
Daniel Stelzer <dsdraco7@gmail.com>
2026-08-30 16:58:29 -0500

    more messing around

--- src/6502/c64_frontend.s
+++ src/6502/c64_frontend.s
@@ -522,13 +522,13 @@ io_mstyle
 	rts
 palette
 	.byt	$0	; normal = black
-	.byt	$2	; reverse = red (warm)
+	.byt	$1	; reverse = white (warm)
 	.byt	$b	; bold = dark gray (light)
-	.byt	$a	; bold reverse = pink (light + warm)
+	.byt	$7	; bold reverse = yellow (light + warm)
 	.byt	$6	; italic = blue (saturated)
-	.byt	$8	; italic reverse = orange (warm + saturated)
+	.byt	$2	; italic reverse = red (warm + saturated)
 	.byt	$e	; italic bold = light blue (saturated + light)
-	.byt	$7	; italic bold reverse = yellow (warm + saturated + light)
+	.byt	$a	; italic bold reverse = pink (warm + saturated + light)
 	.)
 ; available colors:
 ; 0 black 1 white 2 red 3 cyan
